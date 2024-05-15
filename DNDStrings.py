5efd436b48dcd9c5375ff7b089869519cb2e15a9
mrgerwin <jgerwin@otsegoknights.org>
2024-05-15 10:59:46 -0400

    Add files via upload

---
+++ DNDStrings.py
@@ -0,0 +1,66 @@
+"What class do you want to be? (F = Human Fighter, W = Human wizard, D = Dwarf Rogue)"
+"You chose Fighter who is fighting with a longsword and shortbow"
+"You chose wizard who is fighting with a Quarterstaff and Darts"
+"You chose Dwarf who is fighting with a Battleaxe and Crossbow"
+"Your name is " + name
+"Health = " + str(health)
+"Intelligence = " + str(intel)
+"Strength = " + str(strength)
+"Could not recognize your response"
+"The default is Fighter"
+"THE STORY BEGINS"
+"*****************************"
+"""You are teleported to the entrance of a large tower.
+You are told by a mysterious voice that you will face the
+gauntlet. A path of challenges that will either make you more
+experienced or kill you.  If you make it to the end, you win."""
+"There are three paths. (U = Up the Stairs, D = Down the Stairs, S = Straight Down the Hallway)"
+"You went up the stairs"
+"You went down the stairs"
+"You went straight"
+"incorrect input. Restart the game."
+"***********************"
+"You encounter a Rat.  It seems that it has not seen you."
+"Do you wish to A = Attack, or R = Run?"
+"Roll a D-4 and enter it here"
+"Roll a D-6 and enter it here"
+"Roll a D-8 and enter it here"
+"Excellent!  You killed a rat and gained +25XP"
+"You Ran Away from the Rat Continuing up the stairs."
+"Hit enter to Continue"
+"Out of no where you are attacked by a Ghoul.  It attacks first with its claws."
+"Your health is now " + str(health)
+"What do you do? A = Melee Attack, R = Run Away, S = Spell (wizards only)"
+"You can't run away, it is too fast.  You must attack"
+"You try a spell but you are not a wizard.  You are forced to attack."
+"Which spell to cast? C = Cure Wounds, R = Ray of Frost, P = Poison Spray, M = Minor Illusion"
+"Your health increased to " + str(health)
+"Now you must attack"
+"You killed the Ghoul! You gained 200XP!"
+"You continue to the floor of the tower.  You notice an imprint in the floor.  It is a giant webbed footprint."
+"Do you wish to track down the monster or run?  A = Attack, R = Run"
+"The Frog took some damage but you must continue your attack"
+"The illusion creates a large booming sound.  The Frog is not Scared.  You must continue your attack"
+"The Frog is slow to react.  You are able to get off a Melee attack before it pounces."
+"The frog extends its wet tongue.  It wraps around you.  As it pulls you in, you wonder if it can swallow you whole."
+"The frog swallows you whole. Unfortunately no one is available to get you out.  You lost."
+"The frog can only bite you"
+"You managed to kill the frog!  You gain 100XP!"
+"You hear a loud 'Ribbit' followed by a huge mass leaping through the air.  The monster ..."
+"Roll a D-4 to create a speed burst.  Enter result here"
+"You stumble and fall while the frog jumps towards you"
+"You sprint beyond your abilities while the frog jumps towards you"
+"You sprint while the frog jumps towards you."
+"You were eaten by the frog and never made it out of the Gauntlet.  Try again"
+"Your distance from the frog is "
+"Hit Enter to Continue"
+"A large beam of light illuminates the room. You are temporarily blinded.  You lose track of time."
+"You find yourself safe at home.  You realize that you are alive and that you have made it through the gauntlet."
+"The experience has made you a stronger character."
+"You have gained "
+"You have leveled up! +2 proficiency to spend on any of your attributes."
+
+
+
+
+
